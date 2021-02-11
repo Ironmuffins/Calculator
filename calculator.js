5ef9d695b322fa0be1cffecf7a932f6f64a5649f
Jared <midnightfuture@protonmail.com>
2021-02-11 08:37:13 -0800

    Created functions for add, subtract, sum, multiply, divide, get power, and factorial.

---
+++ calculator.js
@@ -0,0 +1,33 @@
+function add (a, b) {
+    return (a + b);	
+    };
+    
+    function subtract (a, b) {
+        return (a - b);
+    };
+    
+    function sum (array) {
+    return array.reduce((accumulator, currentValue) => accumulator + currentValue, 0)
+    };
+    
+    function multiply (array) {
+    return array.reduce((accumulator, currentValue) => accumulator * currentValue)
+    };
+
+    function divide (a, b) {
+        return (a / b);
+    }
+    
+    function power(a, b) {
+        return (a ** b);
+    };
+    
+    function factorial(a) {
+        if (a === 0 || a === 1) {
+            return 1;
+        }
+        else{
+        return (a * factorial(a - 1));
+        } 
+    };
+    
